03a88526e0831b7a411b9f42212755889add15e3
YOUR_NAME <YOUR_EMAIL>
2018-04-04 15:13:07 +0300

    Create MyAOP class.

---
+++ src/main/java/com/veronika/advanced/spring/aop/MyAOP.java
@@ -0,0 +1,15 @@
+package com.veronika.advanced.spring.aop;
+
+public class MyAOP {
+    public MyAOP(){}
+
+    private String s;
+
+    public String getS() {
+        return s;
+    }
+
+    public void setS(String s) {
+        this.s = s;
+    }
+}
